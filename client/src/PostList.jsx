5fa1089cc38d78153bca91f61d26f1290f05cf22
divyuk <div14kes@gmail.com>
2024-05-19 19:30:07 +0530

    The post list shows

--- client/src/PostList.jsx
+++ client/src/PostList.jsx
@@ -1,13 +1,16 @@
 import axios from "axios";
 import { useEffect, useState } from "react";
+import CommentCreate from "./CommentCreate";
+import CommentList from "./CommentList";
+import PostComments from "./PostComments";
 
 function PostList({ posts, setPosts }) {
+  const [commentList, setCommentList] = useState([]);
   useEffect(() => {
     async function getPosts() {
       try {
         const data = await axios.get("http://localhost:4000/posts");
         const postsArray = Object.values(data.data);
-        console.log(postsArray);
         setPosts(postsArray);
       } catch (err) {
         console.log(err);
@@ -19,14 +22,16 @@ function PostList({ posts, setPosts }) {
   return (
     <div>
       <h1 className="m-5 text-3xl font-mono">Your posts</h1>
+
       <ul className="list-none flex flex-wrap justify-center">
         {posts.map((p, index) => (
           <li
             key={index}
-            className="border border-gray-300 bg-white rounded-lg shadow-md m-4 flex justify-center p-4"
-            style={{ width: "200px", height: "150px" }}
+            className="border border-gray-300 bg-white rounded-lg shadow-md m-4  p-4"
           >
-            <span className="text-center">{p.title}</span>
+            <span className="text-lg text-black">{p.title}</span>
+
+            <PostComments postId={p.id} />
           </li>
         ))}
       </ul>
